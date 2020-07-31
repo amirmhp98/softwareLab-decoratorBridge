3524de47dbf788dbd61273a34d0e43df0f447468
Amir Mhp <amir.blueb1377@gmail.com>
2020-08-01 01:05:56 +0430

    Bridge-testsOnly

--- tests/calculator/CalculationTests.java
+++ tests/calculator/CalculationTests.java
@@ -9,15 +9,39 @@ import org.junit.jupiter.api.Test;
 class CalculationTests {
 
     @Test
-    void testA() {
-        MultCalculator multCalculatorA = new autoMultCalculator();
-        PowerCalculator powerCalculatorA = new autoPowerCalculator(multCalculatorA);
+    void testAutoMultAutoPower() {
+        MultCalculator multCalculator = new AutoMultCalculator();
+        PowerCalculator powerCalculator = new AutoPowerCalculator(multCalculator);
 
-		MultCalculator multCalculatorB = new autoMultCalculator();
-		PowerCalculator powerCalculatorB = new autoPowerCalculator(multCalculatorB);
-
-        Assertions.assertEquals("Delicious HouseBlend", beverage.getDescription());
-        Assertions.assertEquals(0.89, beverage.cost());
+        Assertions.assertEquals(81, powerCalculator.pow(3,4));
+        Assertions.assertEquals(125, powerCalculator.pow(5,3));
     }
+
+    @Test
+	void testAutoMultManualPower(){
+		MultCalculator multCalculator = new AutoMultCalculator();
+		PowerCalculator powerCalculator = new ManualPowerCalculator(multCalculator);
+
+		Assertions.assertEquals(28561, powerCalculator.pow(13,4));
+		Assertions.assertEquals(64, powerCalculator.pow(4,3));
+	}
+
+	@Test
+	void testManualMultAutoPower(){
+		MultCalculator multCalculator = new ManualMultCalculator();
+		PowerCalculator powerCalculator = new AutoPowerCalculator(multCalculator);
+
+		Assertions.assertEquals(216, powerCalculator.pow(6,3));
+		Assertions.assertEquals(1728, powerCalculator.pow(12,3));
+	}
+
+	@Test
+	void testManualMultManualPower(){
+		MultCalculator multCalculator = new ManualMultCalculator();
+		PowerCalculator powerCalculator = new ManualPowerCalculator(multCalculator);
+
+		Assertions.assertEquals(49, powerCalculator.pow(7,2));
+		Assertions.assertEquals(2744, powerCalculator.pow(14,3));
+	}
 
 }
